5a2f41ba44679c5961ee874f0040d25237ba0020
Andrii Gegliuk <andrii.gegliuk@gmail.com>
2025-03-07 14:51:21 +0900

    Fix API always returning 0 with TDD

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
-# wsgi.py
 from flask import Flask, jsonify
-from src.dice_roller.dice import Dice
+from dice_roller.dice import Dice
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     dice = Dice()
-    return jsonify({'roll': dice.roll()})
+    roll = dice.roll()
+    return jsonify({'roll': roll})
